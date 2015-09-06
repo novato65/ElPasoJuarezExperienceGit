42d9bb0f10dbb587a8057cb8f475c55f8787bec5
MODESTO VASCO FORNAS <modestovascofornas@MODESTOs-Mini.lan>
2015-09-06 15:04:19 -0600

    Initial commit

---
+++ app/src/main/java/com/solinpromex/elpasojuarezexperience/Inicio.java
@@ -0,0 +1,90 @@
+package com.solinpromex.elpasojuarezexperience;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class Inicio extends AppCompatActivity {
+
+    private Button mexbutton;
+    private Button usabutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+        addListenermexButton();
+        addListenerusaButton();
+    }
+
+    public void addListenermexButton() {
+
+        //Select a specific button to bundle it with the action you want
+
+        mexbutton = (Button) findViewById(R.id.mexButton);
+
+
+        mexbutton.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View view) {
+
+                startActivity(new Intent(Inicio.this, MainActivity.class));
+
+
+            }
+
+        });
+
+    }
+
+    public void addListenerusaButton() {
+
+        //Select a specific button to bundle it with the action you want
+
+        mexbutton = (Button) findViewById(R.id.usaButton);
+
+
+        mexbutton.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View view) {
+
+                Intent openBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.javacodegeeks.com"));
+
+                startActivity(openBrowser);
+
+            }
+
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_inicio, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/solinpromex/elpasojuarezexperience/PrimaryFragmentDormir.java
@@ -0,0 +1,20 @@
+package com.solinpromex.elpasojuarezexperience;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ratan on 7/29/2015.
+ */
+public class PrimaryFragmentDormir extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.primary_layout_dormir,null);
+    }
+}
