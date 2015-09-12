f73b6c263dbfa486371cdf832e174088e4d49648
MODESTO VASCO FORNAS <modestovascofornas@MODESTOs-Mini.lan>
2015-09-11 23:02:28 -0600

    013 mac
    formato como el resto de la app y despues el onclicklistener

--- app/src/main/java/com/solinpromex/elpasojuarezexperience/Detalle_Hotel.java
+++ app/src/main/java/com/solinpromex/elpasojuarezexperience/Detalle_Hotel.java
@@ -44,7 +44,7 @@ public class Detalle_Hotel extends AppCompatActivity {
         tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
             @Override
             public int getIndicatorColor(int position) {
-                return getResources().getColor(R.color.tabsScrollColor);
+                return getResources().getColor(R.color.rojomodesto);
             }
         });
 
