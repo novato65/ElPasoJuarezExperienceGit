cda081ca370d885a58834c14822f23f6a9996765
solinpromex <info@solinpromex.com>
2015-09-14 16:21:17 -0600

    031  DELL
    instalado todo lo necesario para Maps

--- app/src/main/java/com/solinpromex/elpasojuarezexperience/Tab2.java
+++ app/src/main/java/com/solinpromex/elpasojuarezexperience/Tab2.java
@@ -3,18 +3,42 @@ package com.solinpromex.elpasojuarezexperience;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.SupportMapFragment;
+
 
 public class Tab2 extends Fragment {
-    
+
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
+    private GoogleMap map;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.tab_2,container,false);
+        View v = inflater.inflate(R.layout.tab_2, container, false);
         return v;
 
-        //mapa
+
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle state) {
+        super.onActivityCreated(state);
+
+
+
+
     }
 }
