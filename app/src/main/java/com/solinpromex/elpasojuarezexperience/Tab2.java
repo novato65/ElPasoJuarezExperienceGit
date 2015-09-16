e6d8a7b8bddb62cc6ed0fea14e80712050784779
MODESTO VASCO FORNAS <modestovascofornas@MODESTOs-Mini.lan>
2015-09-16 01:32:34 -0600

    mac033
    conseguido inicializar mapa

--- app/src/main/java/com/solinpromex/elpasojuarezexperience/Tab2.java
+++ app/src/main/java/com/solinpromex/elpasojuarezexperience/Tab2.java
@@ -1,6 +1,26 @@
 package com.solinpromex.elpasojuarezexperience;
 
+/**
+ * Created by modestovascofornas on 9/16/15.
+ */
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -8,6 +28,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
@@ -21,8 +43,12 @@ import android.widget.TextView;
 import android.content.Intent;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -33,48 +59,46 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
+public class Tab2 extends Fragment implements OnMapReadyCallback {
 
+    private View view;
+    private Context mContext;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.tab_2, container, false);
 
-public class Tab2 extends Fragment {
+        mContext = getActivity();
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        SupportMapFragment fragment = new SupportMapFragment();
+        transaction.add(R.id.mapView, fragment);
+        transaction.commit();
+
+        fragment.getMapAsync(this);
+
+        return view;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.tab_2, container, false);
-        return v;
+    public void onMapReady(GoogleMap map) {
+        //map is ready
+        // latitude and longitude
+        double latitude = 13.0294278;
+        double longitude =80.24667829999999;
+
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello Maps").icon(BitmapDescriptorFactory.fromResource(R.drawable.estrella));
+
+        // Changing marker icon
+        // marker.icon(BitmapDescriptorFactory
+        // .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+        // adding marker
+        map.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(13.0294278, 80.24667829999999)).zoom(12).build();
+        map.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
     }
-
-    @Override
-    public void onActivityCreated(Bundle state) {
-        super.onActivityCreated(state);
-
-
-    }
-
-
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
-
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-    }
-
-
 }
