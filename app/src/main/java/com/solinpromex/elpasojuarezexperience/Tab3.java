3353b67b7d5f97306f0ee032ecd3ac8d5758fb12
MODESTO VASCO FORNAS <modestovascofornas@MODESTOs-Mini.lan>
2015-09-16 17:27:03 -0600

    mac035
    justo antes de comenzar todos los procesos de tab3, opiniones

--- app/src/main/java/com/solinpromex/elpasojuarezexperience/Tab3.java
+++ app/src/main/java/com/solinpromex/elpasojuarezexperience/Tab3.java
@@ -6,13 +6,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class Tab3 extends Fragment {
-    
+
+    private Integer id_hotel;
+    private TextView label_id_hotel;
+    private Integer hotel_id;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tab_3,container,false);
+
+
         return v;
+    }
+    @Override
+    public void onActivityCreated(Bundle state) {
+        super.onActivityCreated(state);
+
+        label_id_hotel = (TextView) getActivity().findViewById(R.id.hotel);
+
+        hotel_id = getActivity().getIntent().getIntExtra("id_hotel", 0);
+        label_id_hotel.setText(String.valueOf(hotel_id));
+
+
     }
 }
