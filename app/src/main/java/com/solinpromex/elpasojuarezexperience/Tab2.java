6b3f87ca5f7684238d028302799bb9b2117c4861
MODESTO VASCO FORNAS <modestovascofornas@MODESTOs-Mini.lan>
2015-09-15 19:03:28 -0600

    mac032
    conseguido poner test map

--- app/src/main/java/com/solinpromex/elpasojuarezexperience/Tab2.java
+++ app/src/main/java/com/solinpromex/elpasojuarezexperience/Tab2.java
@@ -1,44 +1,80 @@
 package com.solinpromex.elpasojuarezexperience;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.content.Intent;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
-
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 
 public class Tab2 extends Fragment {
 
-    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-    static final LatLng KIEL = new LatLng(53.551, 9.993);
-    private GoogleMap map;
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tab_2, container, false);
         return v;
-
-
     }
-
 
     @Override
     public void onActivityCreated(Bundle state) {
         super.onActivityCreated(state);
 
 
-
-
     }
+
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+
 }
--- app/src/main/java/com/solinpromex/elpasojuarezexperience/Tab3.java
+++ app/src/main/java/com/solinpromex/elpasojuarezexperience/Tab3.java
@@ -12,7 +12,7 @@ public class Tab3 extends Fragment {
     
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.tab_2,container,false);
+        View v = inflater.inflate(R.layout.tab_3,container,false);
         return v;
     }
 }
