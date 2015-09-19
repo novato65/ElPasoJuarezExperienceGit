6516313400be2655c79c448883c2d8da4704316c
MODESTO VASCO FORNAS <modestovascofornas@MODESTOs-Mini.lan>
2015-09-19 02:39:34 -0600

    mac038
    terminado nuevo comentario layout

--- app/src/main/java/com/solinpromex/elpasojuarezexperience/NuevoComentarioHotel.java
+++ app/src/main/java/com/solinpromex/elpasojuarezexperience/NuevoComentarioHotel.java
@@ -1,24 +1,40 @@
 package com.solinpromex.elpasojuarezexperience;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class NuevoComentarioHotel extends AppCompatActivity {
-
-    private TextView hotel_nombre, hotel_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_comentario_hotel);
-
-        //ponemos el nombre del hotel en el textview
-        hotel_nombre = (TextView) findViewById(R.id.nombre_hotel);
-        hotel_nombre.setText(getIntent().getStringExtra("nombre_hotel"));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_nuevo_comentario_hotel, menu);
+        ActionBar actionBar = getActionBar();
+        actionBar.show();
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
