c0264a7e18c543be07de9ccc8b5768077c450527
MODESTO VASCO FORNAS <modestovascofornas@MODESTOs-Mini.lan>
2015-09-19 14:25:41 -0600

    mac039
    mejorado nuevo comentario layout

--- app/src/main/java/com/solinpromex/elpasojuarezexperience/NuevoComentarioHotel.java
+++ app/src/main/java/com/solinpromex/elpasojuarezexperience/NuevoComentarioHotel.java
@@ -5,13 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class NuevoComentarioHotel extends AppCompatActivity {
+public class NuevoComentarioHotel extends AppCompatActivity  {
+
+    private TextView hotel_calificado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_comentario_hotel);
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        hotel_calificado = (TextView) findViewById(R.id.hotel_calificado);
+        hotel_calificado.setText(getIntent().getStringExtra("nombre_hotel"));
         //test 22
         //TEST
     }
